ae92de52a15e9b4ea99d023c126d90e144cd546a
Marcelo <mhbcoelho99@gmail.com>
2024-06-17 16:09:52 -0300

    feature: create new Auth adapter

--- modules/authentication/src/main/adapters/FastifyRouteAdapter.ts
+++ modules/authentication/src/main/adapters/FastifyRouteAdapter.ts
@@ -1,7 +1,14 @@
 import { FastifyReply, FastifyRequest, RouteHandlerMethod } from 'fastify';
 import { UserController } from '../../presentation/controllers/UserController';
+import { AuthController } from '../../presentation/controllers/AuthController';
 
-export const adaptRoute = (controller: UserController, method: keyof UserController): RouteHandlerMethod => {
+export const adaptUserRoute = (controller: UserController, method: keyof UserController): RouteHandlerMethod => {
+  return async (req: FastifyRequest, reply: FastifyReply) => {
+    await controller[method](req, reply);
+  };
+};
+
+export const adaptAuthRoute = (controller: AuthController, method: keyof AuthController): RouteHandlerMethod => {
   return async (req: FastifyRequest, reply: FastifyReply) => {
     await controller[method](req, reply);
   };
