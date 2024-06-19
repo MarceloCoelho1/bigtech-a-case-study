6b6b372ac96892707138e72f6f46e3ffa1d69412
Marcelo <mhbcoelho99@gmail.com>
2024-06-19 08:28:51 -0300

    feat: create forgotPassword route and resetPasswordRoute

--- modules/authentication/src/main/routes/AuthRoutes.ts
+++ modules/authentication/src/main/routes/AuthRoutes.ts
@@ -6,13 +6,17 @@ import { BcryptService } from '../../infra/adapters/BcryptService';
 import { env } from '../../shared/env';
 import { AuthUseCases } from '../../core/domain/usecases/AuthUseCases';
 import { AuthController } from '../../presentation/controllers/AuthController';
+import { MailService } from '../../infra/adapters/MailService';
 
 export const authRouter = (app: FastifyInstance): void => {
   const userRepository = new PrismaUserRepository();
   const jwtService = new JWTService({ secret: env.JWT_SECRET, expiresIn: env.JWT_EXPIRES_IN });
   const bcryptService = new BcryptService();
-  const authUseCases = new AuthUseCases(userRepository, jwtService, bcryptService);
+  const mailService = new MailService();
+  const authUseCases = new AuthUseCases(userRepository, jwtService, bcryptService, mailService);
   const authController = new AuthController(authUseCases);
 
   app.post('/auth/login', adaptAuthRoute(authController, 'login')); 
+  app.post('/auth/forgotpassword', adaptAuthRoute(authController, 'forgotPassword')); 
+  app.post('/auth/resetpassword/:token', adaptAuthRoute(authController, 'resetPassword'))
 } 
