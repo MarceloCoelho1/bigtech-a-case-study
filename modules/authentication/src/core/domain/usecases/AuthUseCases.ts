179d8e7c02ee5bdb77af6cd249cdfda0f494d10c
Marcelo <mhbcoelho99@gmail.com>
2024-06-19 08:27:10 -0300

    feat:forgot password and reset password

--- modules/authentication/src/core/domain/usecases/AuthUseCases.ts
+++ modules/authentication/src/core/domain/usecases/AuthUseCases.ts
@@ -4,13 +4,17 @@ import { IncorrectPassword } from "../errors/IncorrectPassword";
 import { UserNotExists } from "../errors/UserNotExists";
 import { UserRepository } from "../repositories/UserRepository";
 import { ICredentials } from "../types/ICredentials";
+import { IMailService } from "../adapters/mail.interface";
+import { IResetPassword } from "../types/IResetPassword";
+import { InvalidToken } from "../errors/InvalidToken";
 
 
 export class AuthUseCases {
     constructor(
         private userRepository: UserRepository,
         private jwtService: IJwtService,
-        private bcryptService: IBcryptService
+        private bcryptService: IBcryptService,
+        private mailService: IMailService
     ) { }
 
     async login(credentials: ICredentials): Promise<string> {
@@ -21,11 +25,53 @@ export class AuthUseCases {
 
         const isUser = await this.bcryptService.compare(credentials.password, user.passwordHash)
 
-        if(!isUser) {
+        if (!isUser) {
             throw new IncorrectPassword()
         }
 
         const token = this.jwtService.sign({ userId: user.id });
         return token
+    }
+
+    async forgotPassword(email: string): Promise<void> {
+
+        const user = await this.userRepository.findByEmail(email)
+        if (!user) {
+            throw new UserNotExists()
+        }
+        const newToken = this.jwtService.resetToken(user.id)
+
+
+        let data = {
+            to: email,
+            subject: "recovery password",
+            body: newToken
+        }
+        await this.mailService.sendMail(data)
+    }
+
+
+    async resetPassword(data: IResetPassword): Promise<void> {
+        const { token, password } = data
+
+        const verifiedToken = this.jwtService.verify(token)
+        if(!verifiedToken) {
+            throw new InvalidToken()
+        }
+        const user = await this.userRepository.findById(verifiedToken.userId)
+
+        if(!user) {
+            throw new UserNotExists()
+        }
+        
+        let newPasswordHash = await this.bcryptService.hash(password);
+
+        let newUserValues = {
+            ...user,
+            passwordHash: newPasswordHash
+        }
+
+        await this.userRepository.update(newUserValues)
+
     }
 }
