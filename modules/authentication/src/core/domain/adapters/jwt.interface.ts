cb75149ab37ba70af61d3a875dfcdb335c20e223
Marcelo <mhbcoelho99@gmail.com>
2024-06-19 08:24:43 -0300

    feat: reset password body

--- modules/authentication/src/core/domain/adapters/jwt.interface.ts
+++ modules/authentication/src/core/domain/adapters/jwt.interface.ts
@@ -1,6 +1,9 @@
+import { User } from "../entities/User";
+
 export interface IJwtService {
     sign(payload: IJwtServicePayload, expiresIn?: string | number): string;
     verify(token: string): IJwtServicePayload | null;
+    resetToken(id: string): string;
 }
 
 export interface IJwtServicePayload {
