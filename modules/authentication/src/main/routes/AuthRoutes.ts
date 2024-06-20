7dea5dbfbda66815e7c19badf8da22f9d56fc3b1
Marcelo <mhbcoelho99@gmail.com>
2024-06-20 08:50:14 -0300

    route: create auth verify route

--- modules/authentication/src/main/routes/AuthRoutes.ts
+++ modules/authentication/src/main/routes/AuthRoutes.ts
@@ -7,16 +7,19 @@ import { env } from '../../shared/env';
 import { AuthUseCases } from '../../core/domain/usecases/AuthUseCases';
 import { AuthController } from '../../presentation/controllers/AuthController';
 import { MailService } from '../../infra/adapters/MailService';
+import { PrismaUserActivationRepository } from '../../data/repositories/PrismaUserActivationRepository';
 
 export const authRouter = (app: FastifyInstance): void => {
   const userRepository = new PrismaUserRepository();
   const jwtService = new JWTService({ secret: env.JWT_SECRET, expiresIn: env.JWT_EXPIRES_IN });
   const bcryptService = new BcryptService();
   const mailService = new MailService();
-  const authUseCases = new AuthUseCases(userRepository, jwtService, bcryptService, mailService);
+  const userActivationRepository = new PrismaUserActivationRepository()
+  const authUseCases = new AuthUseCases(userRepository, jwtService, bcryptService, mailService, userActivationRepository);
   const authController = new AuthController(authUseCases);
 
   app.post('/auth/login', adaptAuthRoute(authController, 'login')); 
   app.post('/auth/forgotpassword', adaptAuthRoute(authController, 'forgotPassword')); 
-  app.post('/auth/resetpassword/:token', adaptAuthRoute(authController, 'resetPassword'))
+  app.post('/auth/resetpassword', adaptAuthRoute(authController, 'resetPassword'))
+  app.get('/auth/verify', adaptAuthRoute(authController, 'verifyUser'))
 } 
