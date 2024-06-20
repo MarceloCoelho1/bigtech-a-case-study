c890a6b3aebf42088b9b79e098be3be4bdac7087
Marcelo <mhbcoelho99@gmail.com>
2024-06-20 08:46:14 -0300

    feat: create email verification after create account

--- modules/authentication/src/core/domain/usecases/UserUseCases.ts
+++ modules/authentication/src/core/domain/usecases/UserUseCases.ts
@@ -1,7 +1,11 @@
+import { MailService } from '../../../infra/adapters/MailService';
+import { CreateUserDTO } from '../../../presentation/dtos/CreateUserDto';
+import { UpdateUserDto } from '../../../presentation/dtos/UpdateUserDto';
 import { IBcryptService } from '../adapters/bcrypt.interface';
 import { IJwtService } from '../adapters/jwt.interface';
 import { User } from '../entities/User';
 import { UserAlreadyExistsError } from '../errors/UserAlreadyExists';
+import { UserActivationRepository } from '../repositories/UserActivationRepository';
 import { UserRepository } from '../repositories/UserRepository';
 
 
@@ -9,21 +13,45 @@ export class UserUseCases {
   constructor(
     private userRepository: UserRepository,
     private bcryptService: IBcryptService,
-    private jwtService: IJwtService
+    private jwtService: IJwtService,
+    private userActivationRepository: UserActivationRepository,
+    private mailService: MailService
   ) { }
 
-  async createUser(user: User): Promise<{ user: User, token: string }> {
-    const userExist = await this.getUserByEmail(user.email);
+  async createUser(createUserData: CreateUserDTO): Promise<{ user: User }> {
+    const userExist = await this.getUserByEmail(createUserData.email);
 
     if (userExist) {
       throw new UserAlreadyExistsError();
     }
-    user.passwordHash = await this.bcryptService.hash(user.passwordHash);
+    createUserData.password = await this.bcryptService.hash(createUserData.password);
 
-    const createdUser = await this.userRepository.create(user);
-    const token = this.jwtService.sign({ userId: createdUser.id });
+    const createdUser = await this.userRepository.create(createUserData);
+    const userActivationToken = this.jwtService.sign({ userId: createdUser.id });
+    
+    const userActivationDate = {
+      token: userActivationToken,
+      user_id: createdUser.id,
+      expiration_date: new Date(Date.now() + 2 * 60 * 60 * 1000),
+    }
 
-    return { user: createdUser, token };
+    await this.userActivationRepository.create(userActivationDate);
+
+
+    const template = `
+            <p><strong>Recovery your password!</strong>!</p>
+            <a href="http://localhost:3333/auth/verify?token=${userActivationToken}">Click here!</a>
+        `
+
+    let data = {
+      to: createdUser.email,
+      subject: "verify your email",
+      body: template
+    }
+    await this.mailService.sendMail(data)
+
+
+    return { user: createdUser };
   }
 
   async getUserById(id: string): Promise<User | null> {
@@ -34,12 +62,12 @@ export class UserUseCases {
     return this.userRepository.findAll();
   }
 
-  async updateUser(user: User): Promise<User> {
-    return this.userRepository.update(user);
-  }
-
   async deleteUser(id: string): Promise<void> {
     return this.userRepository.delete(id);
+  }
+
+  async updateUser(user: UpdateUserDto): Promise<User> {
+    return this.userRepository.update(user)
   }
 
   async getUserByEmail(email: string): Promise<User | null> {
