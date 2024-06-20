09a8ea616b47264474d0826a1742be595d87f6ec
Marcelo <mhbcoelho99@gmail.com>
2024-06-20 08:45:15 -0300

    fix: change update UserRepository params

--- modules/authentication/src/core/domain/repositories/UserRepository.ts
+++ modules/authentication/src/core/domain/repositories/UserRepository.ts
@@ -1,10 +1,12 @@
+import { CreateUserDTO } from '../../../presentation/dtos/CreateUserDto';
+import { UpdateUserDto } from '../../../presentation/dtos/UpdateUserDto';
 import { User } from '../entities/User';
 
 export interface UserRepository {
-  create(user: User): Promise<User>;
+  create(user: CreateUserDTO): Promise<User>;
   findById(id: string): Promise<User | null>;
   findAll(): Promise<User[]>;
-  update(user: User): Promise<User>;
+  update(user: UpdateUserDto): Promise<User>;
   delete(id: string): Promise<void>;
   findByEmail(email: string): Promise<User | null>;
 }
