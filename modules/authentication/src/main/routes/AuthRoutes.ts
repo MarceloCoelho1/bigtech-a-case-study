88a98a629d228e7c69ba24c7ec4e674645d1e9dc
Marcelo <mhbcoelho99@gmail.com>
2024-06-21 07:55:04 -0300

    feat: create roleMiddleware

--- modules/authentication/src/main/routes/AuthRoutes.ts
+++ modules/authentication/src/main/routes/AuthRoutes.ts
@@ -8,6 +8,8 @@ import { AuthUseCases } from '../../core/domain/usecases/AuthUseCases';
 import { AuthController } from '../../presentation/controllers/AuthController';
 import { MailService } from '../../infra/adapters/MailService';
 import { PrismaUserActivationRepository } from '../../data/repositories/PrismaUserActivationRepository';
+import { roleMiddleware } from '../../presentation/middlwares/roleMiddleware';
+
 
 export const authRouter = (app: FastifyInstance): void => {
   const userRepository = new PrismaUserRepository();
@@ -20,6 +22,8 @@ export const authRouter = (app: FastifyInstance): void => {
 
   app.post('/auth/login', adaptAuthRoute(authController, 'login'));
   app.post('/auth/forgotpassword', adaptAuthRoute(authController, 'forgotPassword'));
-  app.post('/auth/resetpassword', adaptAuthRoute(authController, 'resetPassword'))
-  app.get('/auth/verify', adaptAuthRoute(authController, 'verifyUser'))
+  app.post('/auth/resetpassword', adaptAuthRoute(authController, 'resetPassword'));
+  app.get('/auth/verify', adaptAuthRoute(authController, 'verifyUser'));
+
+  app.get('/auth/admin', {preHandler: [roleMiddleware]}, adaptAuthRoute(authController, 'accessByAdmin'))
 } 
