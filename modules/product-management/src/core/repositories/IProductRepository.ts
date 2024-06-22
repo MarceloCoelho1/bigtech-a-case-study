5750ced60e1dc686e4c10b5b0179d12d9c1beb7d
Marcelo <mhbcoelho99@gmail.com>
2024-06-22 09:01:27 -0300

    feat: create a updateProduct and findById interface

--- modules/product-management/src/core/repositories/IProductRepository.ts
+++ modules/product-management/src/core/repositories/IProductRepository.ts
@@ -1,7 +1,10 @@
 import { CreateProductDTO } from "../../http/dtos/createProductDTO";
+import { UpdateProductDTO } from "../../http/dtos/updateProductDTO";
 import { Product } from "../entities/product";
 
 export interface IProductRepository {
     create(data: CreateProductDTO): Promise<Product>
     getAllProducts(): Promise<Product[]>
+    updateProduct(data: UpdateProductDTO): Promise<Product>
+    findById(id: string): Promise<Product | null>
 }
