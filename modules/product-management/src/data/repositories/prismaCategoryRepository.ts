34f7e27b2767d03d1ea94826aa8c0561cd44908c
Marcelo <mhbcoelho99@gmail.com>
2024-06-23 09:25:19 -0300

    feat: add impl prisma category repository

--- modules/product-management/src/data/repositories/prismaCategoryRepository.ts
+++ modules/product-management/src/data/repositories/prismaCategoryRepository.ts
@@ -2,6 +2,7 @@ import { ICategoryRepository } from "../../core/repositories/ICategoryRepository
 import { CreateCategoryDTO } from "../../http/dtos/createCategoryDTO";
 import { prisma } from "../datasources/prismaClient";
 import { Category } from "../../core/entities/category";
+import { UpdateCategoryDTO } from "../../http/dtos/updateCategoryDTO";
 
 export class PrismaCategoryRepository implements ICategoryRepository {
     async create(data: CreateCategoryDTO): Promise<Category> {
@@ -21,6 +22,33 @@ export class PrismaCategoryRepository implements ICategoryRepository {
     async getCategoryByName(name: string): Promise<Category | null> {
         const category = await prisma.category.findUnique({ where: { name }})
         return category
+    }
+
+    async getCategoryById(id: number): Promise<Category | null> {
+        const category = await prisma.category.findUnique({ where: { id }})
+        return category
+    }
+
+    async updateCategory(data: UpdateCategoryDTO): Promise<Category> {
+        const category = await prisma.category.update({
+            where: { id: data.id },
+            data
+        })
+
+        return category
+    }
+
+    async deleteCategory(id: number): Promise<void> {
+        await prisma.category.delete({ where: { id } })
+    }
+
+    async assignProductsToCategory(data: AssignProductsToCategory): Promise<void> {
+        await prisma.product.update({
+            where: {id: data.productId},
+            data: {
+                category_id: data.categoryId
+            }
+        })
     }
 
 }
