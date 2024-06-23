7b0c88334bc8cf7f941bc1c80535884bf4660074
Marcelo <mhbcoelho99@gmail.com>
2024-06-23 09:23:45 -0300

    feat: add Category repository interface

--- modules/product-management/src/core/repositories/ICategoryRepository.ts
+++ modules/product-management/src/core/repositories/ICategoryRepository.ts
@@ -1,8 +1,13 @@
 import { CreateCategoryDTO } from "../../http/dtos/createCategoryDTO";
+import { UpdateCategoryDTO } from "../../http/dtos/updateCategoryDTO";
 import { Category } from "../entities/category";
 
 export interface ICategoryRepository {
     create(data: CreateCategoryDTO): Promise<Category>;
     getAllCategories(): Promise<Category[]>
     getCategoryByName(name: string): Promise<Category | null>
+    updateCategory(data: UpdateCategoryDTO): Promise<Category>
+    getCategoryById(id: number): Promise<Category | null>
+    deleteCategory(id: number): Promise<void>
+    assignProductsToCategory(data: AssignProductsToCategory): Promise<void>
 }
