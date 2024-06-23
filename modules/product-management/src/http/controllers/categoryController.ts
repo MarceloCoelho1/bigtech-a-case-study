46696b2a0a2bf963789bbfc0a133e1c7f35b9ede
Marcelo <mhbcoelho99@gmail.com>
2024-06-23 09:26:26 -0300

    feat: add category routes and controllers

--- modules/product-management/src/http/controllers/categoryController.ts
+++ modules/product-management/src/http/controllers/categoryController.ts
@@ -2,6 +2,9 @@ import { FastifyReply, FastifyRequest } from "fastify";
 import { CategoryUsecases } from "../../core/usecases/categoryUsecase";
 import { CreateCategoryDTO } from "../dtos/createCategoryDTO";
 import { CategoryAlreadyExists } from "../../core/errors/categoryAlreadyExistsError";
+import { UpdateCategoryDTO } from "../dtos/updateCategoryDTO";
+import { CategoryNotFound } from "../../core/errors/categoryNotFoundError";
+import { ProductNotFound } from "../../core/errors/productNotFoundError";
 
 export class CategoryController {
     constructor(
@@ -28,6 +31,56 @@ export class CategoryController {
             reply.status(201).send({ categories: categories })
         } catch (error) {
             reply.status(500).send({ error: 'Internal Server Error' });
+        }
+    }
+
+    async updateCategory(req: FastifyRequest, reply: FastifyReply) {
+        try {
+            const { id } = req.params as { id: number }
+            const newCategoryData = {
+                ...req.body as UpdateCategoryDTO,
+                id: Number(id)
+            }
+            const category = await this.CategoryUsecases.updateCategory(newCategoryData)
+            reply.status(201).send({ category: category })
+        } catch (error) {
+            if(error instanceof CategoryNotFound) {
+                reply.status(error.statusCode).send({ error: error.message });
+            } else {
+                reply.status(500).send({ error: 'Internal Server Error' });
+            }
+        }
+    }
+
+    async deleteCategory(req: FastifyRequest, reply: FastifyReply) {
+        try {
+            const { id } = req.params as { id: number }
+
+            await this.CategoryUsecases.deleteCategory(Number(id))
+            reply.status(201)
+        } catch (error) {
+            if(error instanceof CategoryNotFound) {
+                reply.status(error.statusCode).send({ error: error.message });
+            } else {
+                reply.status(500).send({ error: 'Internal Server Error' });
+            }
+        }
+    }
+
+    async assignProductsToCategoryController(req: FastifyRequest, reply: FastifyReply) {
+        try {
+            const assignData = req.body as AssignProductsToCategory
+
+            await this.CategoryUsecases.assignProductsToCategory(assignData)
+            reply.status(201)
+        } catch (error) {
+            if (error instanceof CategoryNotFound) {
+                reply.status(error.statusCode).send({ error: error.message });
+            } else if (error instanceof ProductNotFound) {
+                reply.status(error.statusCode).send({ error: error.message });
+            } else {
+                reply.status(500).send({ error: 'Internal Server Error' });
+            }
         }
     }
 
--- modules/product-management/src/http/routes/categoryRoutes.ts
+++ modules/product-management/src/http/routes/categoryRoutes.ts
@@ -2,12 +2,18 @@ import { FastifyInstance } from "fastify";
 import { PrismaCategoryRepository } from "../../data/repositories/prismaCategoryRepository";
 import { CategoryUsecases } from "../../core/usecases/categoryUsecase";
 import { CategoryController } from "../controllers/categoryController";
+import { PrismaProductRepository } from "../../data/repositories/prismaProductRepository";
 
 export const categoryRoutes = (app: FastifyInstance): void => {
     const categoryRepository = new PrismaCategoryRepository()
-    const categoryUsecases = new CategoryUsecases(categoryRepository)
+    const productRepository = new PrismaProductRepository()
+    const categoryUsecases = new CategoryUsecases(categoryRepository, productRepository)
     const categoryController = new CategoryController(categoryUsecases)
 
-    app.get('/category', (req, reply) => categoryController.getAllCategories(req, reply));
+    app.get('/category', (req, reply) => categoryController.getAllCategories(req, reply))
     app.post('/category', (req, reply) => categoryController.createCategory(req, reply))
+    app.patch('/category/:id', (req, reply) => categoryController.updateCategory(req, reply))
+    app.delete('/category/:id', (req, reply) => categoryController.deleteCategory(req, reply))
+    app.patch('/categories/assign-product', (req, reply) => categoryController.assignProductsToCategoryController(req, reply))
+
 }
