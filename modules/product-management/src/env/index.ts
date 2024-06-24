c409bf6c611d699e898c411d14d1b0ec9dc2b037
Marcelo <mhbcoelho99@gmail.com>
2024-06-24 15:11:28 -0300

    env: change env attributes

--- modules/product-management/src/env/index.ts
+++ modules/product-management/src/env/index.ts
@@ -4,6 +4,17 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+
+  // database_url
+  DATABASE_URL: z.string(),
+
+  // firebase
+  API_KEY: z.string(),
+  AUTH_DOMAIN: z.string(),
+  PROJECT_ID: z.string(),
+  STORAGE_BUCKET: z.string(),
+  MESSAGING_SENDER_ID: z.string(),
+  APP_ID: z.string()
 });
 
 const _env = envSchema.safeParse(process.env)
