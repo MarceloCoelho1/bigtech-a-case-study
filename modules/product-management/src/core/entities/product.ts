d0cf3da960c1bbc4dc471843ef9bef16bc18d968
Marcelo <mhbcoelho99@gmail.com>
2024-06-24 18:35:35 -0300

    feat: add new price_with_discount attribute

--- modules/product-management/src/core/entities/product.ts
+++ modules/product-management/src/core/entities/product.ts
@@ -10,7 +10,8 @@ export class Product {
         public category_id: number | null,
         public low_stock_threshold: number,
         public reviews: Review[],
-        public url_image: string | null
+        public url_image: string | null,
+        public price_with_discount: number | null
     ) {}
 }
 
