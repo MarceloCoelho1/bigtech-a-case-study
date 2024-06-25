f296e4894c74107c92253ef370720e00058f676e
Marcelo <mhbcoelho99@gmail.com>
2024-06-25 18:26:37 -0300

    feat: create addProductToCart function

--- modules/order-processing/src/core/usecases/productUsecases.ts
+++ modules/order-processing/src/core/usecases/productUsecases.ts
@@ -1,22 +1,57 @@
+import { AddProductToCart } from "../../http/dtos/addProductToCartDTO";
 import { CreateProductDTO } from "../../http/dtos/createProductDTO";
-import { Product } from "../entities/product";
+import { InvalidToken } from "../errors/invalidTokenError";
+import { ProductNotFound } from "../errors/productNotFoundError";
+import { UserNotExists } from "../errors/userNotExistsError";
+import { ICartProductRepository } from "../repositories/ICartProductRepository";
 import { IProductRepository } from "../repositories/IProductRepository";
+import { IUserRepository } from "../repositories/IUserRepository";
 import { IJwtService } from "../services/IJwtService";
+import { ProductWithoutCarts } from "../types/productWithoutCarts";
 
 
 export class ProductUsecases {
     constructor(
         private productRepository: IProductRepository,
-        private jwtRepository: IJwtService
+        private jwtRepository: IJwtService,
+        private userRepository: IUserRepository,
+        private cartProductRepository: ICartProductRepository
     ) { }
 
-    async create(data: CreateProductDTO): Promise<Product> {
+    async create(data: CreateProductDTO): Promise<ProductWithoutCarts> {
         const product = await this.productRepository.create(data)
         return product
     }
 
-    async getAllProducts(): Promise<Product[]> {
+    async getAllProducts(): Promise<ProductWithoutCarts[]> {
         const products = await this.productRepository.getAllProducts()
         return products
+    }
+
+    async addProductToCart(data: AddProductToCart): Promise<void> {
+        const payload = this.jwtRepository.verify(data.token)
+
+        if(!payload) {
+            throw new InvalidToken()
+        }
+
+        const userId = payload.userId
+
+        const user = await this.userRepository.findById(userId)
+
+        if(!user) {
+            throw new UserNotExists()
+        }
+        const product = await this.productRepository.findById(data.productId)
+
+        if(!product) {
+            throw new ProductNotFound()
+        }
+
+        if(!user.cart) {
+            throw new Error('Internal server Error')
+        }
+
+        await this.cartProductRepository.AddProductToCart(data, user.cart?.id)
     }
 }
--- modules/order-processing/src/data/repositories/prismaProductRepository.ts
+++ modules/order-processing/src/data/repositories/prismaProductRepository.ts
@@ -1,26 +1,28 @@
 import { Product } from "../../core/entities/product";
 import { IProductRepository } from "../../core/repositories/IProductRepository";
+import { ProductWithoutCarts } from "../../core/types/productWithoutCarts";
 import { CreateProductDTO } from "../../http/dtos/createProductDTO";
 import { prisma } from "../datasources/prismaClient";
 
 export class PrismaProductRepository implements IProductRepository {
 
-    async create(data: CreateProductDTO): Promise<Product> {
+    async create(data: CreateProductDTO): Promise<ProductWithoutCarts> {
         const product = await prisma.product.create({
-            data,
-            include: {
-                carts: true
-            }
+            data
         })
         return product
     }
 
-    async getAllProducts(): Promise<Product[]> {
-        const products = await prisma.product.findMany({
-            include: {
-                carts: true
-            }
-        })
+    async getAllProducts(): Promise<ProductWithoutCarts[]> {
+        const products = await prisma.product.findMany()
         return products
+    }
+
+    async findById(id: string): Promise<ProductWithoutCarts | null> {
+        const product = await prisma.product.findUnique({
+            where: {id}
+        })
+
+        return product
     }
 }
