5193cf89978ab482f969cb229d9d55908d094fb4
Marcelo <mhbcoelho99@gmail.com>
2024-06-25 20:57:56 -0300

    feat: add remove item from cart and find product at cart by id interface

--- modules/order-processing/src/core/repositories/ICartProductRepository.ts
+++ modules/order-processing/src/core/repositories/ICartProductRepository.ts
@@ -1,5 +1,8 @@
 import { AddProductToCart } from "../../http/dtos/addProductToCartDTO";
+import { CartProductReturn } from "../types/cartProductReturn";
 
 export interface ICartProductRepository {
     AddProductToCart(data: AddProductToCart, cartId: number): Promise<void>
+    RemoveItemFromCart(productId: string, cartId: number): Promise<void>
+    findProductAtCartById(id: string, cartId: number): Promise<CartProductReturn | null>
 }
