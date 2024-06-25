ad1a7f718ab03b4beca198c4ae1ffc78ed3b552c
Marcelo <mhbcoelho99@gmail.com>
2024-06-25 09:53:46 -0300

    feat: env validation

---
+++ modules/order-processing/src/env/index.ts
@@ -0,0 +1,20 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+
+    // api config
+    NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
+    PORT: z.coerce.number().default(3333),
+
+});
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+    console.error('Invalid environment variables', _env.error.format())
+
+    throw new Error('Invalid environment variable.')
+}
+
+export const env = _env.data
