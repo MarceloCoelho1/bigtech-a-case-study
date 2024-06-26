7c463137919180a671c87dcaaefff172f7c76b3d
Marcelo <mhbcoelho99@gmail.com>
2024-06-26 12:22:52 -0300

    feat: create getAllProductsFromCart interface

--- modules/order-processing/src/core/repositories/ICartRepository.ts
+++ modules/order-processing/src/core/repositories/ICartRepository.ts
@@ -1,5 +1,7 @@
 import { CreateCartDTO } from "../../http/dtos/createCartDTO";
+import { CartProduct } from "../entities/cartProduct";
 
 export interface ICartRepository {
     create(data: CreateCartDTO): Promise<void>
+    getAllProductsFromCart(id: number): Promise<CartProduct[]>
 }
