e7a9142e9d614d49d7ba6bbe7db1a6a220adb07f
Marcelo <mhbcoelho99@gmail.com>
2024-06-26 17:04:40 -0300

    fix: add unit_price to the return

--- modules/order-processing/src/core/repositories/ICartProductRepository.ts
+++ modules/order-processing/src/core/repositories/ICartProductRepository.ts
@@ -2,7 +2,7 @@ import { AddProductToCart } from "../../http/dtos/addProductToCartDTO";
 import { CartProductReturn } from "../types/cartProductReturn";
 
 export interface ICartProductRepository {
-    AddProductToCart(data: AddProductToCart, cartId: number): Promise<void>
+    AddProductToCart(data: AddProductToCart, cartId: number, unit_price: number): Promise<void>
     RemoveItemFromCart(productId: string, cartId: number): Promise<void>
     findProductAtCartById(id: string, cartId: number): Promise<CartProductReturn | null>
     updateProductQuantity(id: string, quantity: number, cartId: number): Promise<void>
