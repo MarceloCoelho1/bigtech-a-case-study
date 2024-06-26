77a97f219f23e0db2595bd7db08f2694d59e29b0
Marcelo <mhbcoelho99@gmail.com>
2024-06-26 09:13:07 -0300

    feat: create updateProductQuantity interface

--- modules/order-processing/src/core/repositories/ICartProductRepository.ts
+++ modules/order-processing/src/core/repositories/ICartProductRepository.ts
@@ -5,4 +5,5 @@ export interface ICartProductRepository {
     AddProductToCart(data: AddProductToCart, cartId: number): Promise<void>
     RemoveItemFromCart(productId: string, cartId: number): Promise<void>
     findProductAtCartById(id: string, cartId: number): Promise<CartProductReturn | null>
+    updateProductQuantity(id: string, quantity: number, cartId: number): Promise<void>
 }
