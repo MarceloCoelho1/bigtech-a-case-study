990f164a7002ebb80d8380beb77701451dc75da4
Marcelo <mhbcoelho99@gmail.com>
2024-06-26 13:27:22 -0300

    feat: create clearCart interface

--- modules/order-processing/src/core/repositories/ICartRepository.ts
+++ modules/order-processing/src/core/repositories/ICartRepository.ts
@@ -4,4 +4,5 @@ import { CartProduct } from "../entities/cartProduct";
 export interface ICartRepository {
     create(data: CreateCartDTO): Promise<void>
     getAllProductsFromCart(id: number): Promise<CartProduct[]>
+    clearCart(id: number): Promise<void>
 }
